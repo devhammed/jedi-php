0428c8cfc9dbbe7482394d79023f884cdbbd300f
Hammed Oyedele <itz.harmid@gmail.com>
2020-10-24 17:55:15 +0100

    docs: add error handling demos for pages and API routes

--- public/index.php
+++ public/index.php
@@ -7,23 +7,47 @@ use Jedi\Application;
 
 $app = new Application();
 
+// Sample error handling logic
 $app->use(function (Context $context, Closure $next) {
     try {
         return $next($context);
     } catch (Throwable $e) {
-        return $context->res->status(500)->send([
-            'ok' => \false,
-            'message' => 'Something bad just happened.',
-        ]);
+        return $context->res->send(
+            'Something bad just happened.',
+            500,
+        );
     }
 });
 
+// Sample group routes
 $app->group('/api', function () use ($app) {
+    // Sample middleware for groups
+    // This also demonstrate nested error handling 😁😁😁
+    $app->use(function (Context $context, Closure $next) {
+        try {
+            return $next($context);
+        } catch (Throwable $e) {
+            return $context->res->send(
+                [
+                    'ok' => \false,
+                    'message' => 'Something bad just happened.',
+                ],
+                500,
+            );
+        }
+    });
+
     $app->get('/', function () {
         return [
             'ok' => true,
             'message' => 'Welcome to our API.',
         ];
+    });
+
+    $app->get('/error', function () {
+        throw new Error('Testing error handling for API');
+
+        return 'Error';
     });
 
     $app->group('/users', function () use ($app) {
@@ -38,12 +62,17 @@ $app->group('/api', function () use ($app) {
 });
 
 $app->get('/', function () {
-    throw new Error('dd');
-    return 'f';
+    return '<h1>Home Page</h1>';
 });
 
 $app->get('/contact', function () {
-    return '<h1>Contact Us</h1>';
+    return '<h1>Contact Us Page</h1>';
+});
+
+$app->get('/error', function () {
+    throw new Error('Testing error handling for pages');
+
+    return 'Error';
 });
 
 $app->fallback(function () {
